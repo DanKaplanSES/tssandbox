6e0477be0645487720da97ff9c08121bf1815b1b
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-01-01 18:18:41 -0800

    document slow-spec-reporter.js; off by default

--- spec/support/slow-spec-reporter.js
+++ spec/support/slow-spec-reporter.js
@@ -1,13 +1,13 @@
 // To enable, add as a helper in your jasmine config file
-var slowSpecsReporter = {
-  specStarted: function(result) {
-    this.specStartTime = Date.now()
-  },
-  specDone: function(result) {
-    var seconds = (Date.now() - this.specStartTime) / 1000
-    if (seconds > 0.5) {
-      console.log('WARNING - This spec took ' + seconds + ' seconds: "' + result.fullName + '"')
-    }
-  },
-}
+const slowSpecsReporter = {
+    specStarted: function(result) {
+      this.specStartTime = Date.now()
+    },
+    specDone: function(result) {
+      const seconds = (Date.now() - this.specStartTime) / 1000
+      if (seconds > 0.5) {
+        console.log('WARNING - This spec took ' + seconds + ' seconds: "' + result.fullName + '"')
+      }
+    },
+  }
 jasmine.getEnv().addReporter(slowSpecsReporter);
